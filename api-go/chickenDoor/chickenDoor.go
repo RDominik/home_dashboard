20222945687308cac7b5ef165341ffab9d84beaa
rieplD <riepl.dominik@gmx.de>
2026-07-27 21:44:34 +0200

    feat: update SetHandler to publish to sleepms topic for engine/sleep command

--- api-go/chickenDoor/chickenDoor.go
+++ api-go/chickenDoor/chickenDoor.go
@@ -213,6 +213,9 @@ func (h *APIHandler) SetHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	topic := fmt.Sprintf("%s/%s/set", nanoSetPrefix, req.Key)
+	if req.Key == "engine/sleep" {
+		topic = fmt.Sprintf("%s/sleepms/set", nanoSetPrefix)
+	}
 	if err := h.mqttManager.Publish(topic, req.Value); err != nil {
 		jsonResponse(w, map[string]any{"ok": false, "error": err.Error()})
 		return
